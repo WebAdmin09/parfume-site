60639332bf22821e53dd1af2ac5c53c221b0ddfc
WebAdmin09 <husanorifjonov9@gmail.com>
2023-11-27 07:16:20 +0500

    first commit

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
             <p className='about-title'>As an independent publication based in the United States, Fragrantica is accessible in multiple languages, welcoming fragrance enthusiasts worldwide. Contribute your insights, expand your knowledge through our articles, and engage in lively discussions on our forums. We foster a respectful and inclusive environment where everyone can share their passion for fragrances.</p>
             <p className='about-title'>Our diverse team of over 45 active members and contributors spans 29 countries, and Fragrantica is published in 18 languages, ensuring a global perspective on the enchanting world of perfumes.
                 Connecting with Fragrantica: Press Relations
-                We're eager to hear from perfume brands of all sizes, so please share your news with us. For efficient communication, email us at editor@fragrantica.com or reach out to team members directly. Providing detailed information allows our editors to seamlessly feature your products in our encyclopedia or news section. To send samples and press materials, please use the following postal address:</p>
+                We`re eager to hear from perfume brands of all sizes, so please share your news with us. For efficient communication, email us at editor@fragrantica.com or reach out to team members directly. Providing detailed information allows our editors to seamlessly feature your products in our encyclopedia or news section. To send samples and press materials, please use the following postal address:</p>
         </div>
     )
 }
